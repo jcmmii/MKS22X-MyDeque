ed64e8b8d8fd37bbb3dd8aef2cc9af7181002006
Justin Chen <jchen06@stuy.edu>
2019-03-21 10:03:54 -0400

    Progress on MyDeque, many methods uncomplete/nonfunctional, needs fixing

--- MyDeque.java
+++ MyDeque.java
@@ -4,16 +4,50 @@ public class MyDeque<E>{
 
   @SuppressWarnings("unchceked")
   public MyDeque() {
+    size = 0;
     data = (E[]new Object[10]);
+    start = 0;
+    end = 10;
   }
-  
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(E element){ }
-  public E removeLast(E element){ }
-  public E getFirst(E element){ }
-  public E getLast(E element){ }
+
+  public MyDeque(int initialCapacity){
+    data = (E[]new Object[initialCapacity]);
+    start = 0;
+    end = initialCapacity;
+  }
+
+  public int size(){
+    return size;
+  }
+
+  public String toString(){
+    String ret = "";
+    for (int x = start; x < initialCapacity; x++) {
+      ret = ret + data[x] + " ";
+    }
+    return ret;
+  }
+
+  public void addFirst(E element){
+    size = size + 1;
+
+  }
+
+  public void addLast(E element){
+    size = size + 1;
+  }
+
+  public E removeFirst(E element){
+    size = size - 1;
+  }
+
+  public E removeLast(E element){
+    size = size - 1;
+  }
+  public E getFirst(E element){
+    return data[0];
+  }
+  public E getLast(E element){
+    return data[last];
+  }
 }
