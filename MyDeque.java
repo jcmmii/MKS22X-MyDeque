7005a1ea9d253d0b8aaafb5a6e4e276260342350
Justin Chen <jchen06@stuy.edu>
2019-03-21 09:43:47 -0400

    create MyDeque, copy/paste methods to fill in

---
+++ MyDeque.java
@@ -0,0 +1,19 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  @SuppressWarnings("unchceked")
+  public MyDeque() {
+    data = (E[]new Object[10]);
+  }
+  
+  public MyDeque(int initialCapacity){  }
+  public int size(){ }
+  public String toString(){ }
+  public void addFirst(E element){ }
+  public void addLast(E element){ }
+  public E removeFirst(E element){ }
+  public E removeLast(E element){ }
+  public E getFirst(E element){ }
+  public E getLast(E element){ }
+}
