e5b282b40e3e30dd08e8ac0563655d1710b7625b
Justin Chen <jchen06@stuy.edu>
2019-03-27 21:59:18 -0400

    deleted all previous broken code, spaces

--- Calculator.java
+++ Calculator.java
@@ -31,74 +31,14 @@ public class Calculator{
       return ret;
     }
 
-/*
-      MyDeque<String> calculation = new MyDeque();
-      MyDeque<String> operations = new MyDeque();
-      MyDeque<String> values = new MyDeque();
-      for (int x = 0; x < s.length(); x++) {
-        if (s.charAt(x) != ' ') {
-          boolean spaceCheck = true;
-          int n = 1;
-          String hold = "";
-          while (spaceCheck) {
-            if (s.charAt(x+n-1)!=' ' && x+n < s.length()) {
-              n++;
-            }
-            else {
-              hold = "" + s.substring(x,x+n);
-              spaceCheck = false;
-              x = x + n;
-              n=1;
-            }
-          }
-          if (checkNumber(hold)) {
-            values.addLast(hold);
-          } else {
-            operations.addLast(hold);
-          }
-        }
+    private static boolean checkNumber(String str) {
+      try {
+        Double.parseDouble(str);
+        return true;
+      } catch (NumberFormatException e) {
+        return false;
       }
-
-*/
- /*
-        if (values.size() >= 2 && operations.size() >= 1) {
-          String op = operations.removeFirst();
-          int firstVal = Integer.parseInt(values.removeFirst());
-          int secVal = Integer.parseInt(values.removeFirst());
-          int goback;
-          if (op == "+") goback = firstVal + secVal;
-          if (op == "-") goback = firstVal - secVal;
-          if (op == "*") goback = firstVal * secVal;
-          if (op == "/") goback = firstVal / secVal;
-          if (op == "%") goback = firstVal & secVal;
-          String gobackStr = goback + "";
-          values.addFirst(gobackStr);
-        }
-
-      }
-*/
-
-      private static boolean checkNumber(String str) {
-        try {
-          Double.parseDouble(str);
-          return true;
-        } catch (NumberFormatException e) {
-          return false;
-        }
-      }
-
-
-
-/*
-      MyDeque<Integer> test = new MyDeque();
-      for (int i = 0; i < s.length(); i++) {
-        String sub = s.substring(i,i+1);
-        if (!sub.equals(" ")) test.addLast(Integer.parseInt(sub));
-      }
-      System.out.println(test);
-      return 0.0; //PLACEHOLDER
     }
-    */
 
     public static void main(String[] args) {
       String string1 = "10 2.0 +";
@@ -110,13 +50,4 @@ public class Calculator{
       System.out.println(eval(string3) + "should be 893");
       System.out.println(eval(string4) + "should be 26");
     }
-
-
 }
-
-
-
-//one MyDeque
-//encounter number add to mydeu
-//take last two numbers for MyDeque
-//do operation //put back
